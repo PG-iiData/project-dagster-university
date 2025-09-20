39e87c6a78ece71632e92fcc2077ab16688effa8
Phillip Gregory <phillip.gregory@ii.co.uk>
2025-09-20 12:13:47 +0100

    Lesson 9: Sensors

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -2,12 +2,20 @@
 import dagster as dg
 from dagster_essentials.defs.partitions import monthly_partition
 
+adhoc_request = dg.AssetSelection.assets(["adhoc_request"])
+
+adhoc_request_job = dg.define_asset_job(
+    name="adhoc_request_job",
+    selection=adhoc_request,
+)
+
+
 trips_by_week = dg.AssetSelection.assets("trips_by_week")
 
 trip_update_job = dg.define_asset_job(
     name="trip_update_job",
     partitions_def=monthly_partition, # partitions added here
-    selection=dg.AssetSelection.all() - trips_by_week
+    selection=dg.AssetSelection.all() - trips_by_week - adhoc_request
 )
 
 weekly_update_job = dg.define_asset_job(
